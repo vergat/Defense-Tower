bc9be0a49d588a0e2d5c2683a4255f8435df0601
gianluca <gianluca.goberti@gmail.com>
2017-06-10 12:53:10 +0200

    -Add UI,Data and Sprite/UI folder -Created RoadSelector and WidgetRoadSelector prefab -Add right and left Arrow sprite -Created a editor script that create a data asset(RoadDataUtils - rightclick on Asset->Create->RoadSelectorData) -Created a script that contain our data(RoadSelectorData - pass this to RoadDataUtils) -Created a script for the road selector widget

---
+++ Assets/Script/RoadSelectorData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSelectorData : ScriptableObject {
+
+    // [SerializeField]
+    // RoadData[] listRoadData;
+   [SerializeField]
+   List<RoadData> listRoadData;
+    public List<RoadData> ListRoadData
+    {
+        get { return listRoadData; }
+    }
+    
+}
+[System.Serializable]
+public class RoadData
+{
+    [SerializeField]
+    private int costo;
+    [SerializeField]
+    Sprite road;
+
+    public int Costo
+    {
+        get { return costo; }
+    }
+
+    public Sprite Road
+    {
+        get { return road; }
+    }
+}
---
+++ Assets/Script/WidgetRoadSelector.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WidgetRoadSelector : MonoBehaviour {
+
+    [SerializeField]
+    RoadSelectorData roadDataAsset;
+
+    [SerializeField]
+    Image mainImageRoad;
+
+    [SerializeField]
+    Image leftImageRoad;
+
+    [SerializeField]
+    Image rightImageRoad;
+
+    [SerializeField]
+    Text roadCost;
+
+    List<RoadData> roadData;
+
+    int currentRoad = 0;
+
+
+    void Start () {
+        
+        roadData = roadDataAsset.ListRoadData;
+        mainImageRoad.sprite = roadData[currentRoad].Road;
+        roadCost.text = roadData[currentRoad].Costo.ToString();
+        mainImageRoad.enabled=true;
+        rightImageRoad.sprite = roadData[(currentRoad + 1)%roadData.Capacity].Road;
+        rightImageRoad.enabled = true;
+        leftImageRoad.sprite = roadData[((roadData.Capacity + currentRoad) - 1) % roadData.Capacity].Road;
+        leftImageRoad.enabled = true;
+    }
+	
+    public void LeftClickEvent()
+    {
+
+        currentRoad = ((roadData.Capacity+currentRoad)-1)% roadData.Capacity;
+        mainImageRoad.sprite = roadData[currentRoad % roadData.Capacity].Road;
+        roadCost.text = roadData[currentRoad % roadData.Capacity].Costo.ToString();
+        rightImageRoad.sprite = roadData[(currentRoad + 1) % roadData.Capacity].Road;
+        leftImageRoad.sprite = roadData[((roadData.Capacity + currentRoad) - 1) % roadData.Capacity].Road;
+    }
+
+    public void RightClickEvent()
+    {
+        currentRoad = ((roadData.Capacity + currentRoad) + 1) % roadData.Capacity;
+        mainImageRoad.sprite = roadData[currentRoad % roadData.Capacity].Road;
+        roadCost.text = roadData[currentRoad % roadData.Capacity].Costo.ToString();
+        leftImageRoad.sprite = roadData[((roadData.Capacity + currentRoad) - 1) % roadData.Capacity].Road;
+        rightImageRoad.sprite = roadData[(currentRoad + 1) % roadData.Capacity].Road;
+    }
+
+    public void Onclick()
+    {
+        Debug.Log("ciao culo");
+    }
+}
