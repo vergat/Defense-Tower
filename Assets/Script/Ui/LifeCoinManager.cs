8c63f12daf4929665e84894844e8f33fdda91e6a
gianluca <gianluca.goberti@gmail.com>
2017-10-23 10:41:21 +0200

    -Health UI

---
+++ Assets/Script/Ui/LifeCoinManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCoinManager : MonoBehaviour {
+    [SerializeField]
+    private Sprite halfHeart;
+    [SerializeField]
+    private Sprite emptyHeart;
+    [SerializeField]
+    private GameObject life;
+
+    private int hitPoint=10;
+	// Use this for initialization
+	void Awake () {
+      // Component[] gife= life.GetComponentsInChildren();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
